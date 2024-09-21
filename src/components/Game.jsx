493d676a179bbc7e2f2196257623cff25074f947
Noman <pxycknomdictator@outlook.com>
2024-09-22 01:23:47 +0500

    added game logic

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,17 @@
 import rock from "/images/icon-rock.svg";
 import paper from "/images/icon-paper.svg";
 import scissor from "/images/icon-scissors.svg";
+import { useContext } from "react";
+import { gameStore } from "../store/gameStore";
 
 export const Game = () => {
+  const { handleUserChoice } = useContext(gameStore);
+
   return (
     <div className="w-full flex items-center gap-10 justify-center mt-20 text-center text-2xl font-semibold">
       <div>
         <img
+          onClick={() => handleUserChoice("rock")}
           className="red-shadow bg-gray-200 hover:bg-gray-300 border-8 border-red-500 rounded-full size-[200px] p-8 cursor-pointer"
           src={rock}
           alt="rock"
@@ -15,6 +20,7 @@ export const Game = () => {
       </div>
       <div>
         <img
+          onClick={() => handleUserChoice("paper")}
           className="green-shadow bg-gray-200 hover:bg-gray-300 border-8 border-green-500 rounded-full size-[200px] p-8 cursor-pointer"
           src={paper}
           alt="paper"
@@ -23,6 +29,7 @@ export const Game = () => {
       </div>
       <div>
         <img
+          onClick={() => handleUserChoice("scissors")}
           className="blue-shadow bg-gray-200 hover:bg-gray-300 border-8 border-blue-500 rounded-full size-[200px] p-8 cursor-pointer"
           src={scissor}
           alt="scissors"
--- src/store/GameStoreProvider.jsx
+++ src/store/GameStoreProvider.jsx
@@ -2,5 +2,34 @@ import { gameStore } from "./gameStore.js";
 
 // eslint-disable-next-line react/prop-types
 export const GameStoreProvider = ({ children }) => {
-  return <gameStore.Provider value={{}}>{children}</gameStore.Provider>;
+  const handlePlayGame = (computerChoice, userChoice) => {
+    if (
+      (userChoice === "rock" && computerChoice === "scissors") ||
+      (userChoice === "paper" && computerChoice === "rock") ||
+      (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log("User win");
+    } else if (userChoice === computerChoice) {
+      console.log("Game draw");
+    } else {
+      console.log("Computer win");
+    }
+  };
+
+  const handleComputerChoice = () => {
+    const choices = ["rock", "paper", "scissors"];
+    const randomChoice = Math.floor(Math.random() * 3);
+    return choices[randomChoice];
+  };
+
+  const handleUserChoice = (userChoice) => {
+    const computerChoice = handleComputerChoice();
+    handlePlayGame(computerChoice, userChoice);
+  };
+
+  return (
+    <gameStore.Provider value={{ handleUserChoice }}>
+      {children}
+    </gameStore.Provider>
+  );
 };
