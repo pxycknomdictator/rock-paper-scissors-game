7f5fc0f2924033619b2943d74446eeb39ac9a906
Noman <pxycknomdictator@outlook.com>
2024-09-22 00:12:22 +0500

    added rock paper scissors

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,3 +1,25 @@
+import rock from "/images/icon-rock.svg";
+import paper from "/images/icon-paper.svg";
+import scissor from "/images/icon-scissors.svg";
+
 export const Game = () => {
-  return <div>Game</div>;
+  return (
+    <div className="w-full flex items-center gap-7 justify-center mt-10">
+      <img
+        className="bg-gray-200 border-8 border-red-500 rounded-full size-[200px] p-8"
+        src={rock}
+        alt="rock"
+      />
+      <img
+        className="bg-gray-200 border-8 border-green-500 rounded-full size-[200px] p-8"
+        src={paper}
+        alt="paper"
+      />
+      <img
+        className="bg-gray-200 border-8 border-blue-500 rounded-full size-[200px] p-8"
+        src={scissor}
+        alt="scissors"
+      />
+    </div>
+  );
 };
